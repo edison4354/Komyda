e83d3909c499d1f6eb5ec0b42be8e013084f5f1e
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-07-14 15:40:00 -0700

    Updated mongodb connection uri

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 // REQUIRED LIBRARIES
-require('dotenv').config();
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config({ path: "./config.env" });
 
 // APP SETUP
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+
+// get driver connection
+const dbo = require("./db/conn");
  
-app.listen(PORT)
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
